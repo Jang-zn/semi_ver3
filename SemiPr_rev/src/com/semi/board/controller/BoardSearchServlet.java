31cc625fb8909da74d7228b3fb229dcb2512c869
yoodonghyun96 <jaja5280@naver.com>
2021-05-28 17:48:33 +0900

    전체회원 게시판 검색기능 구현
    servlet 추가
    검색에 따른 servlet 생성
    
    jsp 수정
    분류에따른 검색기능
    검색하고 데이터를 가져와서도 검색유지

---
+++ SemiPr_rev/src/com/semi/board/controller/BoardSearchServlet.java
@@ -0,0 +1,55 @@
+package com.semi.board.controller;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+import com.semi.board.model.service.*;
+import com.semi.board.model.vo.*;
+
+/**
+ * Servlet implementation class BoardSearchServlet
+ */
+@WebServlet("/board/boardSearch")
+public class BoardSearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardSearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String searchType = request.getParameter("searchType");
+		String searchKeyword = request.getParameter("searchKeyword");
+		
+		//List<Board> list = new BoardService().boardsearch(searchType,searchKeyword,cPage,numPerpage);
+		request.setAttribute("searchKeyword", searchKeyword);
+		request.setAttribute("searchType", searchType);
+		//request.setAttribute("BoardList", list);
+		request.getRequestDispatcher("/views/board/boardList.jsp").forward(request, response);
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
