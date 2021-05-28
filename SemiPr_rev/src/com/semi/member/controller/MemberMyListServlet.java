dd2039b11f9b4a590cec0c0f6afb520192cf2df4
hyup7 <guq3434@gmail.com>
2021-05-28 17:31:47 +0900

    no message

--- SemiPr_rev/src/com/semi/member/controller/MemberMyListServlet.java
+++ SemiPr_rev/src/com/semi/member/controller/MemberMyListServlet.java
@@ -1,11 +1,13 @@
 package com.semi.member.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+import com.semi.member.model.vo.*;
 
 /**
  * Servlet implementation class MemberMyListServlet
@@ -26,6 +28,11 @@ public class MemberMyListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberExcList mel = new MemberExcList();
+		
+		List<MemberExcList> list =new ArrayList();
+		
+		
 		request.getRequestDispatcher("/views/member/plan/memberMyList.jsp").forward(request, response);
 	}
 
